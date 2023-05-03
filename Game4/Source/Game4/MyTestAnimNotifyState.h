67095bf2407d99234b1bd178bee2b76972089734
Daeho Choi <112150244+DaeHOHoHOHo@users.noreply.github.com>
2023-05-03 16:02:40 +0900

    Game5 create

---
+++ Game4/Source/Game4/MyTestAnimNotifyState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotifyState.h"
+#include "MyTestAnimNotifyState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAME4_API UMyTestAnimNotifyState : public UAnimNotifyState
+{
+	GENERATED_BODY()
+	
+};
